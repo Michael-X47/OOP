608cf0f50e2e85b1354e88207a7cecf4b6539832
Michael-X47 <MickeyKiller3@gmail.com>
2024-10-08 12:28:25 +0300

    Project

--- Customers/company.py
+++ Customers/company.py
@@ -4,7 +4,6 @@ from Customers.customer import Customer
 class Company(Customer):
     def __init__(self, customer_id=None, customer_name=None, customer_phone=None,
                  customer_address=None, contact=None, discount=None):
-        # None to let the variable optional to enter it or no
         self.__contact = contact
         self.__discount = discount
         super().__init__(customer_id, customer_name, customer_phone, customer_address)
